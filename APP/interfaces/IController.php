e7bd60548a5e870d413442985c97cf33137bc95c
IvanGrigorov <ivangrigorov9@gmail.com>
2016-03-09 19:32:26 +0100

    Add Interfaces

---
+++ APP/interfaces/IController.php
@@ -0,0 +1,19 @@
+<? 
+
+/**
+ * Interface IController
+ * Interface for all Controller classes
+ */
+interface IController {
+
+	// Default method rendering the view 
+	public function index();
+
+	// Logout functions 
+	public function logOut();
+
+}
+
+
+
+?>
---
+++ APP/interfaces/IModel.php
@@ -0,0 +1,17 @@
+<? 
+
+/**
+ * Interface IModel
+ * Interface for all Model classes
+ */
+interface IModel {
+
+	public function GetDatabase();
+
+	public function SetDatabase($value);
+
+}
+
+
+
+?>
